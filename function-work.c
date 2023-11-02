da12beea48b17df6bed9d40542b6888a97f5a5ac
Parmar Harsh <125750021+Infinite3108@users.noreply.github.com>
2023-11-02 13:20:55 +0530

    Update function-work.c

--- function-work.c
+++ function-work.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include"worker.h"
+
 void main(){
 	float a,b,c;
-	int C;
+	int A,B,C,fac,arr[50];
+	char s1,s2;
 	printf("1. sum of 2 numbers\n");
 	printf("2. from max from 2 numbers\n");
 	printf("3. from min from 2 numbers\n");
@@ -10,6 +12,9 @@ void main(){
 	printf("5. find max from 3 numbers\n");
 	printf("6. get swaped values only\n");
 	printf("7. swap numbers in actual\n");
+	printf("8. Get factorial of numbers\n");
+	printf("9. print array\n");
+	printf("10. get length of string\n");
 	printf("\nenter number for task:\n");
 	scanf("%d",&C);
 	switch(C){
@@ -53,6 +58,22 @@ void main(){
 			swap(&a,&b);
 			printf("a, b: %.2f, %.2f",a,b);
 			break;
+		
+		case 8:
+			printf("enter number:\n");
+			scanf("%d",&A);
+			fac=fact(A);
+			printf("Factorial of %d : %d ",A,fac);
+			break;
+			
+		case 9:
+			ar(arr);
+			break;
+		case 10:
+			printf("enter a string : \n");
+			scanf("\n%[^\n]s",s1);
+			printf("Length of string is: %d",len_str(s1));
+			break; 
 		default: 
 		printf("enter a valid task number");
 	}
